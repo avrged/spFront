7b852660124e68211f007dbca263e3f294bceb0b
DAXTER1829 <diegoazarate110800@gmail.com>
2025-06-27 18:40:22 -0600

    firstJS

---
+++ Scripts/loginAdmin.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("registroForm");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const correo = document.getElementById("correo");
+    const contrasena = document.getElementById("contrasena");
+
+    const errorCorreo = document.getElementById("error-correo");
+    const errorContrasena = document.getElementById("error-contrasena");
+
+    let formularioValido = true;
+
+    errorCorreo.textContent = "";
+    errorContrasena.textContent = "";
+
+    correo.classList.remove("input-error");
+    contrasena.classList.remove("input-error");
+
+    if (correo.value.trim() === "") {
+      errorCorreo.textContent = "*Este campo es obligatorio.";
+      correo.classList.add("input-error");
+      formularioValido = false;
+    }
+
+    if (contrasena.value.trim() === "") {
+      errorContrasena.textContent = "*Este campo es obligatorio.";
+      contrasena.classList.add("input-error");
+      formularioValido = false;
+    }
+
+  });
+});
